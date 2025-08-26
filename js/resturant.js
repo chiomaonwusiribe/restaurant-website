c1f23d1d51146419a409e9dbdd44a10db3e51b38
Chioma Onwusiribe <chiomauzoma478@gmail.comgit config --global user.email>
2025-08-26 18:38:44 +0100

    initial commit

---
+++ js/resturant.js
@@ -0,0 +1,22 @@
+// const tween = KUTE.fromTo(
+//     '#blob1',
+//     {path:'#blob1'},
+//     {repeat:999, duration:3000, yoyo:true}
+// )
+// tween.start()
+
+let lightBtn = document.querySelector(".light")
+let darkBtn = document.querySelector(".dark")
+let htmlBodyTag = document.querySelector("body")
+let paragraphColor = document.querySelectorAll("p")
+
+lightBtn.onclick = function(){
+    htmlBodyTag.style.background = "#f9fafc"
+}
+darkBtn.onclick = function(){
+    htmlBodyTag.style.background = "#12343b"
+    paragraphColor.style.color ="#fff"
+}
+function signIn() {
+    window.location.href = "signin.html"
+}
